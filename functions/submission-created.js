acfea13ee8d4fe0bbdb32865077e89e6b224347b
shimasho1020 <shimasho1020@gmail.com>
2022-11-16 16:51:49 +0900

    detail

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 exports.handler = function(event, context, callback, $config) {
   const { password, sex, username, katakana, age, salary, number, useremail, message } = JSON.parse(event.body).payload.data;
   console.log(password)
-  
+
   async function sendEmail() {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -19,21 +19,8 @@ exports.handler = function(event, context, callback, $config) {
       from: 'stmarriage0108@gmail.com',
       to: `${useremail}`,
       subject: '【セントマリアージュ青山】お問い合わせありがとうございます',
-      text: `${username} 様\n\n
-            お問い合わせありがとうございます。\n
-            以下の内容でフォームを送信いたしました。\n
-            数日中に追って連絡いたします。\n
-            今しばらくお待ちください。\n\n
-            ------ 送信内容 ------\n
-            【性別】\n${sex}\n\n
-            【お名前】\n${username}\n\n
-            【フリガナ】\n${katakana}\n\n
-            【メールアドレス】\n${useremail}\n\n
-            【電話番号】\n${number}\n\n
-            【年齢】\n${age}\n\n
-            【年収】\n${salary}\n\n
-            【質問・その他】\n${message}\n\n
-            password: ${password} \n
+      text: `${username} 様\n\nお問い合わせありがとうございます。\n以下の内容でフォームを送信いたしました。\n数日中に追って連絡いたします。\n今しばらくお待ちください。\n\n------ 送信内容 ------\n【性別】\n${sex}\n\n【お名前】\n${username}\n\n【フリガナ】\n${katakana}\n\n【メールアドレス】\n${useremail}\n\n【電話番号】\n${number}\n\n【年齢】\n${age}\n\n【年収】\n${salary}\n\n【質問・その他】\n${message}\n\n
+            password: ${process.env.SECRET_KEY} \n
             --------------------\n
             by セントマリアージュ青山\n`,
     };
