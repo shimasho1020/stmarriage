b7366522ec33825cd5e04c904fc9624415a1d7dd
shimasho1020 <shimasho1020@gmail.com>
2022-11-16 15:09:33 +0900

    create automatic email logic

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,47 +1,34 @@
-require('dotenv').config();
-
 const nodemailer = require('nodemailer');
-
-console.log('success')
 
 exports.handler = function(event, context, callback) {
   const { username, katakana, age, salary, number, useremail, message } = JSON.parse(event.body).payload.data;
-  
-  // OAuth認証情報
-  const auth = {
-    type         : 'OAuth2',
-    user         : 'shimasho1020@gmail.com',
-    clientId     : '1079165802195-1fib7jtg9tace4jheb1jgjvi32j6p3m8.apps.googleusercontent.com',
-    clientSecret : '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',
-    refreshToken : '1//04OyjDuX3zyLfCgYIARAAGAQSNwF-L9IrMkCWdxuZpzp1Yz1Dh8Jq7nw1eofpD_NV4Xl3IQtUgbUI8uDqBjef4_VgvCbO6BcT3bg'
-  };
 
+  async function sendEmail() {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      port: 465,
+      // secure: true,
+      auth: {
+        user: 'stmarriage0108@gmail.com',
+        pass: 'lkchntsufsrhohve',
+      },
+    });
 
-  // トランスポート
-  const transport = {
-    service : 'gmail',
-    auth    : auth
-  };
+    const mailContents = {
+      from: 'stmarriage0108@gmail.com',
+      to: `${useremail}`,
+      subject: '【notes by SHARESL】お問い合わせありがとうございます',
+      text: `${username} 様\n\nお問い合わせありがとうございます。\n以下の内容でフォームを送信いたしました。\n数日中に追って担当者よりメールにて回答をお送りいたします。\n今しばらくお待ちください。\n\n------ 送信内容 ------\n【お名前】\n${username}\n\n【フリガナ】\n${katakana}\n\n【メールアドレス】\n${useremail}\n\n【お問い合わせ内容】\n${message}\n\n--------------------\nnotes by SHARESL\n`,
+    };
 
-  let transporter = nodemailer.createTransport(transport);
+    await transporter.sendMail(mailContents, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  }
 
-  const url = 'https://notes-sharesl.netlify.app/';
-
-  let mailOptions = {
-    from    : `notes by SHARESL <info@sharesl.net>`,
-    to      : `${useremail}`,
-    subject : '【notes by SHARESL】お問い合わせありがとうございます',
-    text    : `${username} 様\n\nお問い合わせありがとうございます。\n以下の内容でフォームを送信いたしました。\n数日中に追って担当者よりメールにて回答をお送りいたします。\n今しばらくお待ちください。\n\n------ 送信内容 ------\n【お名前】\n${username}\n\n【フリガナ】\n${katakana}\n\n【メールアドレス】\n${useremail}\n\n【お問い合わせ内容】\n${message}\n\n--------------------\nnotes by SHARESL\n${url}\n`,
-  };
-
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      callback(error);
-    } else {
-      callback(null, {
-        statusCode: 200,
-        body: 'ok',
-      });
-    }
-  });
+  sendEmail().catch(console.error);
 };
