9851117e9446a119d8fe2f81d0e94087362e6164
shimasho1020 <shimasho1020@gmail.com>
2022-11-16 15:18:57 +0900

    test

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 
 exports.handler = function(event, context, callback) {
+  console.log('sent gmail')
   const { username, katakana, age, salary, number, useremail, message } = JSON.parse(event.body).payload.data;
 
   async function sendEmail() {
@@ -16,7 +17,7 @@ exports.handler = function(event, context, callback) {
 
     const mailContents = {
       from: 'stmarriage0108@gmail.com',
-      to: `${useremail}`,
+      to: 'shimasho19991020@gmail.com',
       subject: '【notes by SHARESL】お問い合わせありがとうございます',
       text: `${username} 様\n\nお問い合わせありがとうございます。\n以下の内容でフォームを送信いたしました。\n数日中に追って担当者よりメールにて回答をお送りいたします。\n今しばらくお待ちください。\n\n------ 送信内容 ------\n【お名前】\n${username}\n\n【フリガナ】\n${katakana}\n\n【メールアドレス】\n${useremail}\n\n【お問い合わせ内容】\n${message}\n\n--------------------\nnotes by SHARESL\n`,
     };
