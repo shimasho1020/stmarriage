5aeb9b7d2714f7b80f975a013ce863edb2eb0432
shimasho1020 <shimasho1020@gmail.com>
2022-11-16 15:28:58 +0900

    detail

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -16,7 +16,7 @@ exports.handler = function(event, context, callback) {
 
     const mailContents = {
       from: 'stmarriage0108@gmail.com',
-      to: 'shimasho19991020@gmail.com',
+      to: `${useremail}`,
       subject: '【notes by SHARESL】お問い合わせありがとうございます',
       text: `${username} 様\n\nお問い合わせありがとうございます。\n以下の内容でフォームを送信いたしました。\n数日中に追って担当者よりメールにて回答をお送りいたします。\n今しばらくお待ちください。\n\n------ 送信内容 ------\n【お名前】\n${username}\n\n【フリガナ】\n${katakana}\n\n【メールアドレス】\n${useremail}\n\n【お問い合わせ内容】\n${message}\n\n--------------------\nnotes by SHARESL\n`,
     };
