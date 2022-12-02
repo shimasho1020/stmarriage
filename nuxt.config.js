81aee9b095c596c3ea354b5150546c46fb283360
shimasho1020 <shimasho1020@gmail.com>
2022-12-03 02:09:55 +0900

    detail

--- nuxt.config.js
+++ nuxt.config.js
@@ -132,13 +132,20 @@ export default {
       '@nuxtjs/firebase',
       {
         config: {
-          apiKey: process.env.VUE_APP_API_KEY,
-          authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-          projectId: process.env.VUE_APP_PROJECT_ID,
-          storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-          messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-          appId: process.env.VUE_APP_ID,
-          measurementId: process.env.VUE_MEASUREMENT_ID,
+          // apiKey: VUprocess.env.E_APP_API_KEY,
+          // authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+          // projectId: process.env.VUE_APP_PROJECT_ID,
+          // storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+          // messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+          // appId: process.env.VUE_APP_ID,
+          // measurementId: process.env.VUE_APP_MEASUREMENT_ID,
+          apiKey: "AIzaSyBlsf86qWsZPIlV6hqEh7J3PIu-aa8hmi0",
+          authDomain: "stmarriage-database.firebaseapp.com",
+          projectId: "stmarriage-database",
+          storageBucket: "stmarriage-database.appspot.com",
+          messagingSenderId: "1079165802195",
+          appId: "1:1079165802195:web:2b39de34622bdb890d2626",
+          measurementId: "G-84XCX1D1F6"
         },
         services: {
           // auth: {
@@ -202,8 +209,15 @@ export default {
   },
 
   publicRuntimeConfig: {
-    baseURL: process.env.BASE_URL || 'http://locahost:3000',
-    apiURL: process.env.API_URL || 'http://localhost:3333',
+    firebaseConfig : {
+      apiKey: process.env.VUE_APP_API_KEY,
+      authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+      projectId: process.env.VUE_APP_PROJECT_ID,
+      storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+      appId: process.env.VUE_APP_ID,
+      measurementId: process.env.VUE_APP_MEASUREMENT_ID,
+    }
   },
 
   privateRuntimeConfig: {
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -10,7 +10,14 @@ const firebaseConfig = {
   storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_ID,
-  measurementId: process.env.VUE_MEASUREMENT_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
+  // apiKey: "AIzaSyBlsf86qWsZPIlV6hqEh7J3PIu-aa8hmi0",
+  // authDomain: "stmarriage-database.firebaseapp.com",
+  // projectId: "stmarriage-database",
+  // storageBucket: "stmarriage-database.appspot.com",
+  // messagingSenderId: "1079165802195",
+  // appId: "1:1079165802195:web:2b39de34622bdb890d2626",
+  // measurementId: "G-84XCX1D1F6",
 }
 
 export const firebaseApp = initializeApp(firebaseConfig);
