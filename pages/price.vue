2f6669bd3093b02d5ebff0068cca2fd805d2cca4
shimasho1020 <shimasho1020@gmail.com>
2022-12-03 17:16:58 +0900

    delete unnecessary image

--- pages/price.vue
+++ pages/price.vue
@@ -129,20 +129,20 @@ let isDisplay = computed(() => {
 const courses = [
   {
     name:'フルサポートコース',
-    img:'~/assets/images/black_wedding.webp',
+    img:'/images/black_wedding.webp',
     note:'充実したサポート内容のおススメコースです。当社の多くの会員様がこちらのコースで活動されています。',
     href:'#full',
   },
   {
     name:'カジュアルコース',
-    img:'~/assets/images/wedding-table-desserts.webp',
+    img:'/images/wedding-table-desserts.webp',
     // img:'/images/marriage-gate.webp',
     note:'婚活にまだ迷いのある方、とりあえずお試しした方におすすめのコースです。追加料金でフルサポートコースに変更可能です。',
     href:'#casual',
   },
   {
     name:'カウンセリングコース',
-    img: "~/assets/images/counseling.webp",
+    img: "/images/counseling.webp",
     note:'他社で婚活中の方、プライベートな恋愛でお悩みの方が気軽に電話で相談できるコースです。一人で悩んでいないでご相談ください。',
     href:'#counseling',
   },
