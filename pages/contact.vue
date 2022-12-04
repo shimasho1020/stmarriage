f2ce525ebdadb31f8077d4a375d97545e45682f4
shimasho1020 <shimasho1020@gmail.com>
2022-12-04 12:13:50 +0900

    detail

--- pages/contact.vue
+++ pages/contact.vue
@@ -46,8 +46,8 @@
       </div>
       <div class="p-contact__item">
         <label for="date">面談希望日付<span class="necessary">(必須)</span></label><br>
-        <date-picker class="date_input" id="date" name="date" v-model="date" format='yyyy-MM-dd' placeholder="日付を選択してください"></date-picker>
-        <input type="hidden" name="formatDate" v-model="formatDate">
+        <date-picker class="date_input" id="date" v-model="date" format='yyyy-MM-dd' placeholder="日付を選択してください"></date-picker>
+        <input type="hidden" name="date" v-model="formatDate">
       </div>
       <div class="p-contact__item">
         <label for="message">質問・その他</label><br>
@@ -150,7 +150,7 @@ const onSubmit = () => {
   params.append('number', number.value);
   params.append('useremail', useremail.value);
   params.append('method', method.value);
-  params.append('formatDate', formatDate.value);
+  params.append('date', formatDate.value);
   params.append('message', message.value);
   if(botField.value){
     params.append('bot-field', botField.value);
