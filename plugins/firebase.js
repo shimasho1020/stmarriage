8a578eba6a043d598f8cc273168fd575af6b5c3a
shimasho1020 <shimasho1020@gmail.com>
2023-02-03 21:16:19 +0900

    correct detail

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,22 +2,23 @@ import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+import { useContext } from '@nuxtjs/composition-api'
 
 const firebaseConfig = {
+  // apiKey: process.env.FB_API_KEY,
+  // authDomain: process.env.FB_AUTH_DOMAIN,
+  // projectId: process.env.FB_PROJECT_ID,
+  // storageBucket: process.env.FB_STORAGE_BUCKET,
+  // messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  // appId: process.env.FB_ID,
+  // measurementId: process.env.FB_MEASUREMENT_ID,
   apiKey: "AIzaSyBlsf86qWsZPIlV6hqEh7J3PIu-aa8hmi0",
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-  appId: process.env.FB_ID,
-  measurementId: process.env.FB_MEASUREMENT_ID,
-  // apiKey: "AIzaSyBlsf86qWsZPIlV6hqEh7J3PIu-aa8hmi0",
-  // authDomain: "stmarriage-database.firebaseapp.com",
-  // projectId: "stmarriage-database",
-  // storageBucket: "stmarriage-database.appspot.com",
-  // messagingSenderId: "1079165802195",
-  // appId: "1:1079165802195:web:2b39de34622bdb890d2626",
-  // measurementId: "G-84XCX1D1F6",
+  authDomain: "stmarriage-database.firebaseapp.com",
+  projectId: "stmarriage-database",
+  storageBucket: "stmarriage-database.appspot.com",
+  messagingSenderId: "1079165802195",
+  appId: "1:1079165802195:web:2b39de34622bdb890d2626",
+  measurementId: "G-84XCX1D1F6",
 }
 
 export const firebaseApp = initializeApp(firebaseConfig);
