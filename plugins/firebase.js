7419559b0c76e84ff15d96e2c7dd0d2c1f29f253
shimasho1020 <shimasho1020@gmail.com>
2023-02-04 00:34:12 +0900

    complete env logic

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -11,20 +11,9 @@ const firebaseConfig = {
   messagingSenderId: process.env.messagingSenderId,
   appId: process.env.appId,
   measurementId: process.env.measurementId,
-  // apiKey: "AIzaSyBlsf86qWsZPIlV6hqEh7J3PIu-aa8hmi0",
-  // authDomain: "stmarriage-database.firebaseapp.com",
-  // projectId: "stmarriage-database",
-  // storageBucket: "stmarriage-database.appspot.com",
-  // messagingSenderId: "1079165802195",
-  // appId: "1:1079165802195:web:2b39de34622bdb890d2626",
-  // measurementId: "G-84XCX1D1F6",
 }
 
 export const firebaseApp = initializeApp(firebaseConfig);
-
-// export default ({app}) => {
-//   console.log(app.context.env.apiKey)
-// }
 
 export const auth = getAuth(firebaseApp);
 export const storage = getStorage(firebaseApp);
