25f0ef984d2a60cc1fe4d35eb5155409c7251dec
shimasho1020 <shimasho1020@gmail.com>
2023-02-03 23:55:35 +0900

    add nuxt-config with env

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -6,6 +6,12 @@ const contentful = require('contentful')
 const createClient = () => {
   return contentful.createClient({
     apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+    appId: process.env.FB_ID,
+    measurementId: process.env.FB_MEASUREMENT_ID,
   })
 }
 
